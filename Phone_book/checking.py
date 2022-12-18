34b3c3ac05178ec53c1b73669fb5a7ec8e4dc4f6
KirillUvarov <112546441+KirillUvarov@users.noreply.github.com>
2022-12-19 01:04:57 +0300

    Phone_book_add_database_V1.5

--- Phone_book/checking.py
+++ Phone_book/checking.py
@@ -1,4 +1,13 @@
 def checking_for_correctness_name(arg1: str, lenght_str: int):
+    
+    # lenght_user_name = 20 
+    name_str = 'имя'          
+    # name = (checking_for_correctness_name(name_str,lenght_user_name))
+
+    # lenght_user_surname = 40 
+    name_str = 'фамилию'
+    # surname = (checking_for_correctness_name(name_str,lenght_user_surname))
+
     while True:
         try:  
             arg1 = list(input(f'Введите {name_str}\n'))
@@ -8,35 +17,16 @@ def checking_for_correctness_name(arg1: str, lenght_str: int):
                 return (''.join(arg1)).capitalize()     
         except:  
             print('Вы ввели не строку!')
-            
-lenght_user_name = 20 
-name_str = 'имя'          
-name = (checking_for_correctness_name(name_str,lenght_user_name))
 
-lenght_user_surname = 40 
-name_str = 'фамилию'
-surname = (checking_for_correctness_name(name_str,lenght_user_surname))
+checking_for_correctness_name()
+
+# lenght_user_name = 20 
+# name_str = 'имя'          
+# name = (checking_for_correctness_name(name_str,lenght_user_name))
+
+# lenght_user_surname = 40 
+# name_str = 'фамилию'
+# surname = (checking_for_correctness_name(name_str,lenght_user_surname))
 
 # print(f'{name} {surname}')
 
-import database 
-
-# def contact_input():
-#     """
-#     Просит пользователя ввести фамилию, имя, телефон и город,
-#     а потом заносит в базу данных
-
-#     """
-#     id = 22 #Не придумал, как обратиться к последнему элементу, вытащить значение id и прибавить 1
-#     sec_name = str(input("Введите фамилию: "))
-#     first_name = str(input("Введите имя: "))
-#     tel_number = str(input("Введите номер телефона: "))
-#     adress = str(input("Введите адрес: "))
-#     contact_n ={ 
-#         "id": id,
-#         "sec_name": sec_name, 
-#         "first_name": first_name,
-#         "tel_number": tel_number,
-#         "adress": adress
-#     }
-#     return add_to_json(contact_n)
--- Phone_book/database.py
+++ Phone_book/database.py
@@ -65,27 +65,6 @@ def read_file_list():
 # print(read_file_list())
 # print(type(read_file()))
 
-#Выводит на экран приглашение к заполнению данных и вводит в базу
-def contact_input():
-    """
-    Просит пользователя ввести фамилию, имя, телефон и город,
-    а потом заносит в базу данных
-
-    """
-    id = search_last_id()+1 #Не придумал, как обратиться к последнему элементу, вытащить значение id и прибавить 1
-    sec_name = str(input("Введите фамилию: "))
-    first_name = str(input("Введите имя: "))
-    tel_number = str(input("Введите номер телефона: "))
-    address = str(input("Введите адрес: "))
-    contact_n ={ 
-        "id": id,
-        "sec_name": sec_name, 
-        "first_name": first_name,
-        "tel_number": tel_number,
-        "adress": address
-    }
-    return add_to_json(contact_n)
-
 #Поиск значения id в последнем заведённом контакте
 def search_last_id():
     """
@@ -103,13 +82,99 @@ def search_last_id():
 
 # print(search_last_id())
 
+
+def checking_for_correctness_name(arg1: str, lenght_str: int):
+    
+    # lenght_user_name = 20 
+    name_str = 'имя'          
+    # name = (checking_for_correctness_name(name_str,lenght_user_name))
+
+    # lenght_user_surname = 40 
+    name_str = 'фамилию'
+    # surname = (checking_for_correctness_name(name_str,lenght_user_surname))
+
+    while True:
+        try:  
+            arg1 = list(input(f'Введите {name_str}\n'))
+            if len(arg1) > lenght_str:
+                print('Вы вели слишком много символов')  
+            else:  
+                return (''.join(arg1)).capitalize()     
+        except:  
+            print('Вы ввели не строку!')
+    
+# tel_number = list(input('Введите номер телефона в формате +|Код страны|xxx|xxxxxxx|:\n'))
+
+#Проверка корректности введённого номера телефона
+def checking_for_correctness_of_input(arg: str) -> str:
+    lenght_tel = len(arg)
+    plus = arg[:1:lenght_tel-1]
+    body_number = ''.join(arg[1:])
+    result_str = ''
+    if plus[0] != '+':
+        print("Ошибка ввода! Начните вводить с '+'")  
+    elif body_number.isdigit() != True:
+        print("Ошибка ввода! Вы ввели не цифру!")
+    elif len(body_number) > 17:
+        print('Вы ввели слишком много цифр')
+    else:
+        result_str = ''.join(arg) 
+    return result_str
+
+ 
+# print(checking_for_correctness_of_input(tel_number))
+
+
+def check_name(name) -> True:
+    alphabet = 'абвгдежзийклмнопрстуфхцчшщъыьэюяАБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
+    a_2 = list(alphabet)
+    i = name #list(input("Введите фамилию: "))
+    for j in range(0, len(i)):
+        if i[j] in a_2:
+            j+=1
+        else:
+            # print('Вводить можно только русские буквы')
+            return False
+
+
+#Выводит на экран приглашение к заполнению данных и вводит в базу
+def contact_input():
+    """
+    Просит пользователя ввести фамилию, имя, телефон и город,
+    а потом заносит в базу данных
+
+    """
+    alphabet = 'абвгдежзийклмнопрстуфхцчшщъыьэюяАБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
+    a_2 = list(alphabet)
+    id = search_last_id()+1 
+    sec_name = str(input("Введите фамилию: "))
+    if check_name(sec_name) == True:
+        first_name = str(input("Введите имя: "))
+        # tel_number = str(input("Введите номер телефона: "))
+        tel_number = list(input('Введите номер телефона в формате +|Код страны|xxx|xxxxxxx|:\n'))
+        address = str(input("Введите адрес: "))
+        contact_n ={ 
+                "id": id,
+                "sec_name": sec_name, 
+                "first_name": first_name,
+                "tel_number": tel_number,
+                "adress": address
+            }
+
+        return add_to_json(contact_n)
+    else: 
+        print('Вводить можно только русские буквы')
+
+contact_input() #Выводит на экран приглашение к заполнению данных и вводит в базу
+
+
 # create_json()
 
 # add_to_json(contact_1) #Добавляем заданную карточку
 # add_to_json(contact_2)
 # add_to_json(contact_3)
 
-contact_input() #Выводит на экран приглашение к заполнению данных и вводит в базу
+# contact_input() #Выводит на экран приглашение к заполнению данных и вводит в базу
 
 #Ищем по имени или фамилии. Выводит карточку полностью
 def search_name(name):
